65c877826e75dfd5a8e529caf948da3794dc5f3b
VictorOmarT <victoromar001@hotmail.com>
2021-09-20 11:23:18 -0500

    Vars

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("ola k ase");
+const futures = [
+    "Better Luck next time",
+    "Thats not going to happend",
+    "Maybe next time",
+    "Yes",
+    "Is not For you",
+    "For sure",
+    "Maybe",
+    "Ask me again",
+    "No",
+    "Only if you really work for it",
+    "Ask me Tomorrow",
+    "Be careful with that",
+    "everything is going to work",
+    "thats really a mystery",
+    "The answer is yes",
+    "The answer is no",
+    "trust no one",
+    "that will kill you",
+    "i can see happiness in the future",
+    "keep your mind and hope",
+
+];
+const Angel = "The Angels say: ";
+const Evil = "The Demons say ";
+
+
